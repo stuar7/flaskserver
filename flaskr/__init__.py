18476148f6ac6abbefbf3a8627b51f1516132720
stuartpm <stuartpm@gmail.com>
2022-09-21 00:36:17 +0800

    Clean up + Default Image Update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, request, jsonify #added to top of file
 from flask_cors import CORS #added to top of file
 
-from .db import get_db
 import sqlite3
 
 def create_app(test_config=None):
@@ -32,16 +31,6 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-  #  @app.route('/api/carbay', methods=['GET'])
-  #  def api_get_users():
-  #      return jsonify(get_carbay())
-
-    @app.route('/api/carbay/update',  methods = ['PUT'])
-    def api_update_carbay():
-        carbay = request.get_json()
-        print(carbay)
-        return jsonify(update_carbay(carbay))
-
     from . import db
     db.init_app(app)
 
@@ -52,52 +41,10 @@ def create_app(test_config=None):
     app.register_blueprint(carpark.bp)
     app.add_url_rule('/', endpoint='index')
 
+    @app.route('/api/carbay/update',  methods = ['PUT'])
+    def api_update_carbay():
+        carbay = request.get_json()
+        print(carbay)
+        return jsonify(carpark.update_carbay(carbay))
+
     return app
-
-def update_carbay(bay):
-    updated_carbay = {}
-    try:
-        conn = db.get_db()
-        cur = conn.cursor()
-        cur.execute("UPDATE carpark SET pos1 = ?, pos2 = ?, height = ?, width = ?, colour = ? WHERE id = ?",  
-                     (bay["pos1"], bay["pos2"], bay["height"], 
-                     bay["width"], bay["colour"], 
-                     bay["id"],))
-        conn.commit()
-        #return the user
-        updated_carbay = get_carbay_by_id(bay["id"])
-        print("carbay" + updated_carbay)
-    except Exception as inst:
-        print(inst)
-        print(inst.args)
-        print((type(inst)))
-        print("are we getting an exception")
-        conn.rollback()
-        updated_user = {}
-    finally:
-        conn.close()
-    print("Did we get here")
-    return updated_carbay
-
-
-def get_carbay_by_id(bay_id):
-    carbay = {}
-    try:
-        conn = db.get_db()
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM carpark WHERE id = ?", 
-                       (bay_id,))
-        row = cur.fetchone()
-
-        # convert row object to dictionary
-        carbay["id"] = row["id"]
-        carbay["pos1"] = row["pos1"]
-        carbay["pos2"] = row["pos2"]
-        carbay["width"] = row["width"]
-        carbay["height"] = row["height"]
-        carbay["colour"] = row["colour"]
-    except:
-        carbay = {}
-
-    return carbay
