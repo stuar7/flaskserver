b64df529230aece217e9ff5282a59eaa0a132163
stuartpm <stuartpm@gmail.com>
2022-10-13 22:18:06 +0800

    Code Cleanup + Better Dummy Data + Updated README.md - Removed unused images - Removed unused libraries

--- parkr/db.py
+++ parkr/db.py
@@ -12,7 +12,6 @@ def get_db():
 
     return g.db
 
-
 def close_db(e=None):
     db = g.pop('db', None)
 
@@ -21,10 +20,8 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
-
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
 
 @click.command('init-db')
 def init_db_command():
