52ca52ef024016881174cb1d6eca861515c87768
stuartpm <stuartpm@gmail.com>
2022-10-13 23:43:55 +0800

    Code cleanup

--- parkr/analysis.py
+++ parkr/analysis.py
@@ -24,31 +24,35 @@ def timeOfDayBetween(s, e, c):
 @bp.route('/c/<string:carparkname>/analysis', methods=['GET', 'POST'])
 def analysis(carparkname="carpark"):
 
-    startDate = "2022-09-01"
-    endDate = datetime.today().strftime('%Y-%m-%d')
-   
-    try:
-        datemin = datetime.strptime(request.args.get('start-date'), '%Y-%m-%d').timestamp()
-        startDate = request.args.get('start-date')
-    except:
-        datemin = None
-    try:
-        datemax = datetime.strptime(request.args.get('end-date'), '%Y-%m-%d').timestamp()
-        endDate = request.args.get('end-date')
-    except:
-        datemax = None
+    # Default start date on page load (today - 30 days)
+    startDate = (datetime.today() - timedelta(days=30))
+    endDate = datetime.today()
 
-    startTime = datetime.strptime("8:30", '%H:%M')
-    endTime = datetime.strptime("17:30", '%H:%M')
-    timeIsSet = False
+    # Default time
+    startTime = datetime.strptime("8:30:00", '%H:%M:%S')
+    endTime = datetime.strptime("17:30:00", '%H:%M:%S')
+    # Try and parse GET time messages
     try:
         startTime = datetime.strptime(request.args.get('start-time'), '%H:%M:%S')
         endTime = datetime.strptime(request.args.get('end-time'), '%H:%M:%S')
-        timeIsSet = True
     except Exception as inst:
-        timeIsSet = False
         pass
 
+    # Try and parse GET date messages
+    try:
+        datemin = datetime.strptime(f"{request.args.get('start-date')}-{startTime}", '%Y-%m-%d-%H:%M%S').timestamp()
+        startDate = datetime.strptime(request.args.get('start-date'), '%Y-%m-%d')
+    except:
+        startDate = startDate + timedelta(hours=startTime.hour, minutes=startTime.minute)
+        datemin = startDate.timestamp()
+    try:
+        datemax = datetime.strptime(f"{request.args.get('end-date')}-{endTime}", '%Y-%m-%d-%H:%M:%S').timestamp()
+        endDate = datetime.strptime(request.args.get('end-date'), '%Y-%m-%d')
+    except:
+        endDate = endDate + timedelta(hours=endDate.hour, minutes=endDate.minute)
+        datemax = endDate.timestamp()
+
+    # Construct the date comparison for SQL lookup
     dateComparison = f""
     if(datemin):
         dateComparison += f" AND datetime(date, 'unixepoch', 'localtime') >= datetime({datemin}, 'unixepoch', 'localtime')"
@@ -58,6 +62,10 @@ def analysis(carparkname="carpark"):
         print(f"Date max: {datemax}")
     print(dateComparison)
 
+    # Graph datapoint and label variables
+    statusBins = {'BAYS_EMPTY': [], 'BAYS_FULL': [], 'BAYS_UNKNOWN': []}
+    dayBinLabels = []
+    # Fetch all our snapshot
     try:
         db = get_db()
         snapshot = db.execute(
@@ -69,11 +77,12 @@ def analysis(carparkname="carpark"):
         description = thiscarpark['description']
     
         snapshot = [dict(i) for i in snapshot]
+        # If the SQL fetch did not return anything, return empty page.
         if(len(snapshot) == 0):
             return render_template('carpark/analysis.html',carparkname=carparkname, 
-        dayBinEmpty=None, dayBinLabels=None,
-        startDate=startDate, endDate=endDate, description=description,
-        valuesCalculated=None,
+        dayBinLabels=dayBinLabels,
+        startDate=startDate.strftime('%Y-%m-%d'), endDate=endDate.strftime('%Y-%m-%d'), description=description,
+        valuesCalculated=None, statusBins=statusBins,
         startTime=datetime.strptime(f'{startTime.hour}:{startTime.minute}:00', '%H:%M:%S').strftime("%H:%M:%S"),
         endTime=datetime.strptime(f'{endTime.hour}:{endTime.minute}:00', '%H:%M:%S').strftime("%H:%M:%S"))
         
@@ -85,8 +94,6 @@ def analysis(carparkname="carpark"):
     numberOfBays = snapshot[0]['BAYS_EMPTY'] + snapshot[0]['BAYS_FULL'] +snapshot[0]['BAYS_UNKNOWN']
     # Per Day
     currentDay = datetime.fromtimestamp(snapshot[0]['date']).date()
-    dayBinLabels = []
-    statusBins = {'BAYS_EMPTY': [], 'BAYS_FULL': [], 'BAYS_UNKNOWN': []}
     dayBinIndex = 0
     currentDivisor = 1
     # Fill all the variables with the first entry to distinguish when the day has changed.
@@ -118,26 +125,18 @@ def analysis(carparkname="carpark"):
             valuesCalculated += 1
             currentDivisor += 1
         else:
-            if(timeIsSet):
+            # Previous Entry Time
+            pTP = datetime.fromtimestamp(previousEntryDate)
+            # Add 29 minutes to the previous entry time
+            ptP_end = pTP + timedelta(minutes=29)
+            # If this entry is within 29 minutes of the previous, we ignore it
+            if(isCurrentInTimePeriod(time(pTP.hour, pTP.minute), time(ptP_end.hour, ptP_end.minute), 
+                    time(cET.hour, cET.minute)) == False):
                 # Checks to see if time is between hour/minute range
-                if(timeOfDayBetween(
-                    time(startTime.hour, startTime.minute), 
+                if(timeOfDayBetween(time(startTime.hour, startTime.minute), 
                     time(endTime.hour, endTime.minute), 
                     time(cET.hour, cET.minute))):
                     # This entry falls inside our allowed time range
-                    statusBins['BAYS_EMPTY'][dayBinIndex] += entry['BAYS_EMPTY']
-                    statusBins['BAYS_FULL'][dayBinIndex] += entry['BAYS_FULL']
-                    statusBins['BAYS_UNKNOWN'][dayBinIndex] += entry['BAYS_UNKNOWN']
-                    valuesCalculated += 1    
-                    currentDivisor += 1                
-            else:
-                # Previous Entry Time
-                pTP = datetime.fromtimestamp(previousEntryDate)
-                # Add 29 minutes to the previous entry time
-                ptP_end = pTP + timedelta(minutes=29)
-                # If this entry is within 29 minutes of the previous, we ignore it
-                if(isCurrentInTimePeriod(time(pTP.hour, pTP.minute), time(ptP_end.hour, ptP_end.minute), 
-                        time(cET.hour, cET.minute)) == False):
                     statusBins['BAYS_EMPTY'][dayBinIndex] += entry['BAYS_EMPTY']
                     statusBins['BAYS_FULL'][dayBinIndex] += entry['BAYS_FULL']
                     statusBins['BAYS_UNKNOWN'][dayBinIndex] += entry['BAYS_UNKNOWN']
@@ -149,12 +148,12 @@ def analysis(carparkname="carpark"):
         statusBins['BAYS_EMPTY'][dayBinIndex] /= currentDivisor
         statusBins['BAYS_FULL'][dayBinIndex] /= currentDivisor
         statusBins['BAYS_UNKNOWN'][dayBinIndex] /= currentDivisor
-        if(statusBins['BAYS_EMPTY'][dayBinIndex] + statusBins['BAYS_FULL'][dayBinIndex]+statusBins['BAYS_UNKNOWN'][dayBinIndex] != numberOfBays):
+        if(statusBins['BAYS_EMPTY'][dayBinIndex] + statusBins['BAYS_FULL'][dayBinIndex]+statusBins['BAYS_UNKNOWN'][dayBinIndex] < numberOfBays):
             statusBins['BAYS_UNKNOWN'][dayBinIndex] += numberOfBays - (statusBins['BAYS_EMPTY'][dayBinIndex] + statusBins['BAYS_FULL'][dayBinIndex]+statusBins['BAYS_UNKNOWN'][dayBinIndex])
 
     return render_template('carpark/analysis.html',carparkname=carparkname, 
         dayBinLabels=dayBinLabels,
-        startDate=startDate, endDate=endDate, description=description,
+        startDate=startDate.strftime('%Y-%m-%d'), endDate=endDate.strftime('%Y-%m-%d'), description=description,
         valuesCalculated=valuesCalculated, 
         statusBins=statusBins, numberOfBays=numberOfBays,
         startTime=datetime.strptime(f'{startTime.hour}:{startTime.minute}:00', '%H:%M:%S').strftime("%H:%M:%S"),
